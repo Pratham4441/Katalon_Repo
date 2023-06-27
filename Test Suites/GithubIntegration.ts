bccbf638c8d74123aa036afd5541ce97b29b4a43
Prathamesh.Hulavale <Prathamesh.Hulavale@DS019.step.com>
2023-06-27 16:13:12 +0530

    All New Github commit

---
+++ Test Suites/GithubIntegration.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GithubIntegration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ae41880b-f367-4def-96a2-bd113dca9d82</testSuiteGuid>
+   <testCaseLink>
+      <guid>b1534e62-311b-4e8f-bbba-f077be13ae8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/for_Ech</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
