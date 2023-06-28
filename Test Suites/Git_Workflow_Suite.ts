4db420aaa4cc9ca0ef78e2c4b149fdbe8b0aac4c
prathamesh.hulavale@steponestepahead.com <Prathamesh.Hulavale@DS019.step.com>
2023-06-28 14:11:43 +0530

    third Commit

---
+++ Test Suites/Git_Workflow_Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Git_Workflow_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>80222fa8-b1ed-45ac-a769-0ecd9907ce2b</testSuiteGuid>
+   <testCaseLink>
+      <guid>23f9dd48-2af3-4a28-81bd-93e25ac7dac9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_Git_workflow</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
