41373e182ebe34e16039e70025566475018f8acd
prathamesh.hulavale@steponestepahead.com <Prathamesh.Hulavale@DS019.step.com>
2023-06-28 12:03:49 +0530

    Second commit 28/06/2023

---
+++ Test Suites/New_suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New_suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a894c8e8-edd4-433e-bf5f-4c93d3515dc9</testSuiteGuid>
+   <testCaseLink>
+      <guid>d24ad85a-939b-4f82-9a36-699177624fe2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HelloWorld</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
